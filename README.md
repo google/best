b93bf42136226a50b57db3708f83dbe5145c3444
Daniel Liu <danielliuu@google.com>
2022-06-22 23:49:25 +0000

    Use poetry and pyformat

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # BAM Error Stats
 Analysis of error types in BAM files.
+
+## Setup
+1. Install [poetry](https://python-poetry.org/docs/#installation).
+2. Run `poetry install` in this repo.
+3. Run this tool using `poetry run python bam-error-stats`.
--- bam-error-stats/__main__.py
+++ bam-error-stats/__main__.py
@@ -5,6 +5,8 @@ Helps with analyzing the output of CCS and DeepConsensus.
 
 from collections.abc import Sequence
 
+from dataclasses import dataclass
+
 from multiprocessing import Pool
 import argparse
 
@@ -33,16 +35,21 @@ def run(input_path: str, aln_stats_path: str, processes: int, chunk_size: int):
   with pysam.AlignmentFile(input_path, "rb", check_sq=False) as bam_file:
     with open(aln_stats_path, "w") as aln_stats_file:
       with Pool(processes=processes) as p:
-        for aln_stats in p.imap_unordered(get_aln_stats, bam_file.fetch(), chunk_size):
+        for aln_stats in p.imap_unordered(get_aln_stats, bam_file.fetch(),
+                                          chunk_size):
           aln_stats_file.write(aln_stats.to_csv())
           aln_stats_file.write("\n")
 
 
-if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description="Report alignment error and concordance stats from a BAM file.", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+if __name__ == "__main__":
+  parser = argparse.ArgumentParser(
+      description="Report alignment error and concordance stats from a BAM file.",
+      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument("input", help="Input BAM file")
-  parser.add_argument("aln_stats", help="Output CSV file with per alignment statistics")
+  parser.add_argument(
+      "aln_stats", help="Output CSV file with per alignment statistics")
   parser.add_argument("-t", "--threads", type=int, default=4, help="Threads")
-  parser.add_argument("--chunk-size", type=int, default=1024, help="Chunk size per thread")
+  parser.add_argument(
+      "--chunk-size", type=int, default=1024, help="Chunk size per thread")
   args = parser.parse_args()
   run(args.input, args.aln_stats, args.threads, args.chunk_size)
