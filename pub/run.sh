ee247dfa99621487e1ac317981f027ab118e730f
danielecook <danielecook@google.com>
2022-09-27 21:19:05 +0000

    add best command for other seq types.

--- pub/run.sh
+++ pub/run.sh
@@ -10,12 +10,13 @@ mkdir reference illumina ont pacbio
 wget -P reference wget -P reference https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.draft_v1.0.fasta.gz
 gzip -dc reference/chm13.draft_v1.0.fasta.gz > reference/chm13.draft_v1.0.fasta
 
-N_LINES=100000
+N_LINES=10000
 REFERENCE=reference/chm13.draft_v1.0.fasta
 
 function download_bam {
-  DIRECTORY=${1}
-  URL=${2}
+  N_LINES=${1}
+  DIRECTORY=${2}
+  URL=${3}
   curl -P ${DIRECTORY} ${URL} | \
   samtools view -h | \
   head -n ${N_LINES} | \
@@ -23,12 +24,18 @@ function download_bam {
 }
 
 # Illumina
-download_bam illumina https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/alignments/chm13.draft_v1.0.pcrfree.bam
+download_bam 100000 illumina https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/alignments/chm13.draft_v1.0.pcrfree.bam
 
 # ONT
-download_bam ont https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/alignments/chm13.draft_v1.0.ont_guppy_3.6.0.wm_2.01.pri.bam
+download_bam 1000 ont https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/alignments/chm13.draft_v1.0.ont_guppy_3.6.0.wm_2.01.pri.bam
 
 # PacBio HiFi
-download_bam pacbio https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/alignments/chm13.draft_v1.0.hifi_20k.wm_2.01.pri.bam
+download_bam 10000 pacbio https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/alignments/chm13.draft_v1.0.hifi_20k.wm_2.01.pri.bam
 
-best illumina/chm13.draft_v1.0.pcrfree.bam reference./
+#==========#
+# Run Best #
+#==========#
+
+best illumina/chm13.draft_v1.0.pcrfree.bam ${REFERENCE} illumina/illumina
+best ont/chm13.draft_v1.0.ont_guppy_3.6.0.wm_2.01.pri.bam ${REFERENCE} ont/ont
+best pacbio/chm13.draft_v1.0.hifi_20k.wm_2.01.pri.bam ${REFERENCE} pacbio/pacbio
