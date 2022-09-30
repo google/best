4c7a716e81d6d89182239171ffe7649f320e058e
Daniel Liu <danielliuu@google.com>
2022-09-30 00:23:51 +0000

    Random sampling for pub script

--- pub/run.sh
+++ pub/run.sh
@@ -8,10 +8,8 @@ mkdir reference illumina ont pacbio
 
 # Download the CHM13 v1.0 draft
 wget -P reference wget -P reference https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.draft_v1.0.fasta.gz
-gzip -dc reference/chm13.draft_v1.0.fasta.gz > reference/chm13.draft_v1.0.fasta
 
-N_LINES=10000
-REFERENCE=reference/chm13.draft_v1.0.fasta
+REFERENCE=reference/chm13.draft_v1.0.fasta.gz
 
 function download_bam {
   N_LINES=${1}
@@ -19,7 +17,7 @@ function download_bam {
   URL=${3}
   curl -P ${DIRECTORY} ${URL} | \
   samtools view -h | \
-  head -n ${N_LINES} | \
+  shuf -n ${N_LINES} | \
   samtools view -bh > ${DIRECTORY}/$(basename ${URL})
 }
 
