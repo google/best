75598d18e2e8f86c496f5673fb98144c39ab0f74
Daniel Liu <danielliuu@google.com>
2022-09-30 20:38:40 +0000

    Fix best cli params for pub script

--- pub/run.sh
+++ pub/run.sh
@@ -7,6 +7,6 @@
 REFERENCE=reference/chm13.draft_v1.0.fasta.gz
 ARGS="-t 4 --no-per-aln-stats --intervals-hp --bin-types gc_content:0.05"
 
-best ${ARGS} --bin-types q_len:10 illumina/chm13.draft_v1.0.pcrfree.bam ${REFERENCE} illumina/illumina
-best ${ARGS} --bin-types q_len:10000 ont/chm13.draft_v1.0.ont_guppy_3.6.0.wm_2.01.pri.bam ${REFERENCE} ont/ont
-best ${ARGS} --bin-types q_len:1000 pacbio/chm13.draft_v1.0.hifi_20k.wm_2.01.pri.bam ${REFERENCE} pacbio/pacbio
+best ${ARGS} --bin-types q_len:10 -- illumina/chm13.draft_v1.0.pcrfree.bam ${REFERENCE} illumina/illumina
+best ${ARGS} --bin-types q_len:10000 -- ont/chm13.draft_v1.0.ont_guppy_3.6.0.wm_2.01.pri.bam ${REFERENCE} ont/ont
+best ${ARGS} --bin-types q_len:1000 -- pacbio/chm13.draft_v1.0.hifi_20k.wm_2.01.pri.bam ${REFERENCE} pacbio/pacbio
