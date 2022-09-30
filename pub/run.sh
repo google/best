5ee08c4d2139f39a0c5a94d402243f449a668663
Daniel Liu <danielliuu@google.com>
2022-09-30 20:47:24 +0000

    Print per aln stats

--- pub/run.sh
+++ pub/run.sh
@@ -5,7 +5,7 @@
 #==========#
 
 REFERENCE=reference/chm13.draft_v1.0.fasta.gz
-ARGS="-t 4 --no-per-aln-stats --intervals-hp --bin-types gc_content:0.05"
+ARGS="-t 4 --intervals-hp --bin-types gc_content:0.05"
 
 best ${ARGS} --bin-types q_len:10 -- illumina/chm13.draft_v1.0.pcrfree.bam ${REFERENCE} illumina/illumina
 best ${ARGS} --bin-types q_len:10000 -- ont/chm13.draft_v1.0.ont_guppy_3.6.0.wm_2.01.pri.bam ${REFERENCE} ont/ont
