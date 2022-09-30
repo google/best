615ffa4a0bf02f2dc3daff2f2f0df9bf8d95f0cf
Daniel Liu <danielliuu@google.com>
2022-09-30 04:58:14 +0000

    Allow wget to overwrite

--- pub/run.sh
+++ pub/run.sh
@@ -7,7 +7,7 @@
 mkdir reference illumina ont pacbio
 
 # Download the CHM13 v1.0 draft
-wget -P reference https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.draft_v1.0.fasta.gz
+wget -P reference -O chm13.draft_v1.0.fasta.gz https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/assemblies/chm13.draft_v1.0.fasta.gz
 
 REFERENCE=reference/chm13.draft_v1.0.fasta.gz
 
