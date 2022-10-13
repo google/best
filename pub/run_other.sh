32be87de80ebf5ba4ef00e1439dcbf5fdd9f9644
Daniel Liu <danielliuu@google.com>
2022-10-13 22:04:22 +0000

    Fix script

--- pub/run_other.sh
+++ pub/run_other.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-REFERENCE=reference/chm13.draft_v1.0.fasta.gz
+REFERENCE=reference/chm13.draft_v1.0.fasta
 BAM_FILE=pacbio/chm13.draft_v1.0.hifi_20k.wm_2.01.pri.bam
 
 BAM_CONCORDANCE=~/hg002-ccs/concordance/bamConcordance
 echo "bamConcordance"
 time ${BAM_CONCORDANCE} ${REFERENCE} ${BAM_FILE} pacbio/bamConcordance.csv
 
-POMOXIS=python3 ~/pomoxis/pomoxis/stats_from_bam.py
+POMOXIS="python3 ~/pomoxis/pomoxis/stats_from_bam.py"
 echo "pomoxis"
 time ${POMOXIS} ${BAM_FILE} -o pacbio/pomoxis.tsv -s pacbio/pomoxis_summary.txt -t 4
