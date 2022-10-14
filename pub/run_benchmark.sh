596de04e0b88f37e2ca6ac40c77be06fd3576345
Daniel Liu <danielliuu@google.com>
2022-10-14 06:59:30 +0000

    Change benchmark script

--- pub/run_benchmark.sh
+++ pub/run_benchmark.sh
@@ -7,5 +7,11 @@ BAM_CONCORDANCE=~/hg002-ccs/concordance/bamConcordance
 echo "bamConcordance"
 time ${BAM_CONCORDANCE} ${REFERENCE} ${BAM_FILE} pacbio/bamConcordance.csv
 
-echo "pomoxis"
-time stats_from_bam ${BAM_FILE} -o pacbio/pomoxis.tsv -s pacbio/pomoxis_summary.txt -t 4
+#echo "pomoxis"
+#time assess_homopolymers count ${BAM_FILE} -o pacbio/pomoxis -t 4
+
+echo "best 1 thread"
+time best -t 1 ${BAM_FILE} ${REFERENCE} pacbio/best_timing
+
+echo "best 4 thread"
+time best -t 4 ${BAM_FILE} ${REFERENCE} pacbio/best_timing
